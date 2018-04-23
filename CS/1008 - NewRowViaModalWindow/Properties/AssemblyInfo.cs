8641b70cdf8dc64c955da51b60e7b3e8ac0039f5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/1008 - NewRowViaModalWindow/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿// Developer Express Code Central Example:
+// How to add a new row using another modal window
+// 
+// The following example demonstrates how to programmatically add a new row to a
+// GridControl. To see this approach in action, click the "Add new row..." button,
+// fill in three text boxes in the invoked window and click OK.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3058
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_1008___NewRowViaModalWindow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("_1008___NewRowViaModalWindow")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c6f4232-eb98-4f0e-9152-a313b21bb1da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
